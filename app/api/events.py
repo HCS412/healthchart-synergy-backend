690d436a4322cfd0b1a069098f6ac4c66154132c
Brandon Brooks <bbrooksvc@gmail.com>
2025-07-13 19:25:11 -0400

    Update events.py

--- app/api/events.py
+++ app/api/events.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, BackgroundTasks
 from app.models.event import Event
 from app.services.event_handler import handle_event
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import get_db
 
 router = APIRouter()
 
 @router.post("/", tags=["EHR Events"])
-def post_event(event: Event):
-    return handle_event(event)
+async def post_event(event: Event, db: AsyncSession = Depends(get_db), background_tasks: BackgroundTasks):
+    return await handle_event(event, db, background_tasks)
