548414189cb7b9fad1077d56b8815a3ca9b31728
Brandon Brooks <bbrooksvc@gmail.com>
2025-07-13 18:34:47 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,6 +19,8 @@ app.include_router(rooms.router, prefix="/rooms", tags=["Rooms"])
 app.include_router(events.router, prefix="/events", tags=["EHR Events"])
 app.include_router(badges.router, prefix="/badges", tags=["Badge Reader"])
 app.include_router(alerts.router, prefix="/alerts", tags=["Alerts"])
+app.include_router(signage.router, prefix="/signage", tags=["Smart Signage"])
+
 
 @app.get("/")
 def root():
